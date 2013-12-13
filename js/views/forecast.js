c0905736bd68966a4f758b6fb1b8496f3736eeca
garbagemanonfire <nilesvm@gmail.com>
2013-12-12 20:12:42 -0800

    Dialed in Bootstrap, added sunrise sunset max min temp date

--- js/views/forecast.js
+++ js/views/forecast.js
@@ -6,17 +6,30 @@ var ForecastView = Backbone.View.extend({
   initialize: function () {
     this.listenTo(this.model, 'change', this.render);
     this.render();
-    // this.makeWeek;
   },
 
   render: function () {
-    var context = {}
-    context.daily = this.model.get('daily') || {};
+    var context = {};
+    daily = this.model.get('daily') || { data: [] };
+    context.sevenDayForecast = [];
+
+    daily.data.forEach(function (day) {
+      var contextData = {};
+      var timestamp = new Date(day.time * 1000);
+      var sunrise = new Date(day.sunriseTime * 1000);
+      var sunset = new Date(day.sunsetTime * 1000);
+      contextData.date = timestamp.getMonth() + 1 + '/' + timestamp.getDate();
+      contextData.sunRise = sunrise.getHours() + ':' + sunrise.getMinutes();
+      contextData.sunSet = sunset.getHours() + ':' + + sunset.getMinutes();
+      contextData.maxTemp = day.temperatureMax;
+      contextData.minTemp = day.temperatureMin;
+      contextData.summary = day.summary;
+      context.sevenDayForecast.push(contextData)
+    });
+
     this.$el.html(this.template(context));
     return this;
-  }
-
-
+  } 
 
 });
 
